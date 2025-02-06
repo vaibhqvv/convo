716463d92d97c831e48bb92dac3e0b65d6389ea4
vaibhqvv <voidsyntaxes@gmail.com>
2025-02-06 22:46:04 +0530

    implement sign up functionality with jwt token generation

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,13 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body;
     try{
+        if(!fullName || !email || !password){
+            return res.status(400).json({ message: "All fields are required"});
+        }
         if(password.length < 6){
             return res.status(400).json({ message: "Password must be at least 6 characters"});
         }
@@ -22,14 +26,23 @@ export const signup = async (req, res) => {
 
         if(newUser){
             //generate jwt token here
+            generateToken(newUser._id, res);
+            await newUser.save();
 
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
         } else{
             res.status(400).json({ message: "Invalid user data" });
         }
 
 
     } catch(error){
-
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
     }
 };
 
